7986a57ced71f059b39437c67f6ff320e1fa575b
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-01-13 12:57:16 -0800

    Add thread, and network inports to Porgram.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿
+﻿using _4945_A2.Network;
+using _4945_A2.Threads;
+
+
+using Network = _4945_A2.Network.Network;
+using MultiCast = _4945_A2.Network.MulticastNetwork;
+using WebScoket = _4945_A2.Network.WebSocketNetwork;
+
+using Game = _4945_A2.Threads.GameThread;
+using User = _4945_A2.Threads.UserThread;
+
 namespace Program { 
     public class Program
     {
