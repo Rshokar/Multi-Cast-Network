b6cf922ca8ac48b655ac74728a28627558027080
Ravinder Shokar <ravindershokar@gmail.com>
2023-06-08 18:11:39 -0700

    Fixed multi cast between processes. IP adress was wrong

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿using _4945_A2.Network;
-using _4945_A2.Threads;
+﻿using System;
+using System.Threading;
 
 
 using Network = _4945_A2.Network.Network;
 using MultiCast = _4945_A2.Network.MulticastNetwork;
-using WebScoket = _4945_A2.Network.WebSocketNetwork;
 
 using Game = _4945_A2.Threads.GameThread;
 using User = _4945_A2.Threads.UserThread;
@@ -17,20 +16,16 @@ namespace Program {
 
         public static void testOneM()
         {
-            P[] packets = {
-                new P(), new P(), new P(), new P(),
-                new P(), new P(), new P(), new P(), 
-            }; 
-
-
             Game g = new Game();
             Network n = new MultiCast(g);
             User u = new User(n);
 
             n.execute();
 
-            for (int i = 0; i < packets.Length; i++) {
-                n.send(packets[i]);
+            for (int i = 0; i < 10; i++) {
+                P p = new();
+                Thread.Sleep(1000);
+                n.send(p);
             }
             Console.WriteLine("\n\nEND \n\n");
 
@@ -93,9 +88,16 @@ namespace Program {
         }
         public static void Main(string[] args)
         {
-
+            Game g = new Game();
+            if (args.Length > 0) {
+                MultiCast mC = new MultiCast(g);
+                mC.execute();
+                Console.WriteLine("Network listening");
+                Console.WriteLine("Hit enter to end");
+                Console.ReadLine();
+                return;
+            }
             testMulti();
-            //testWeb();
         }
     }
 }
